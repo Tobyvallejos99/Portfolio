8413cdc2beebc2e367c840cd29899ac01f293360
Your Name <you@example.com>
2023-05-18 17:14:30 -0300

    firts

---
+++ src/content_option.js
@@ -0,0 +1,139 @@
+const logotext = "TOBIAS VALLEJOS";
+const meta = {
+    title: "Tobias Vallejos Portfolio",
+    description: "Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Tobias Vallejos",
+    animated: {
+        first: "I love coding",
+        second: "I develop websites ",
+        third: "I code cool websites",
+    },
+    description: "I am a programmer who seeks to expand his knowledge in order to grow as a Professional",
+    your_img_url: "https://c4.wallpaperflare.com/wallpaper/371/264/21/itzmauuuroo-hackers-anonymous-hd-wallpaper-preview.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "My name is Tobias, I am currently a FullStack Developer, with several programming knowledge. I am a passionate programmer and analytical with strong skills in problem solving and teamwork backed by successful projects in which I have demonstrated my ability to obtain results. ",
+};
+const worktimeline = [{
+        jobtitle: "Henry Assistant",
+        where: "SoyHenry",
+        date: "2023",
+    }
+];
+
+const skills = [
+    {
+        name: "JavaScript",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    
+    {
+        name: "Redux",
+        value: 90,
+    },
+    {
+        name: "HTLM",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "NodeJS",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "PHP",
+        value: 85,
+    },
+    {
+        name: "PostgreSQL",
+        value: 85,
+    },
+    {
+        name: "TypeScript",
+        value: 85,
+    },
+    {
+        name: "MongoDB",
+        value: 80,
+    },
+    
+    
+];
+
+const services = [{
+        title: "Websites Design",
+        description: "Designing attractive and functional websites is essential to standing out online. A good design considers visual aesthetics, usability and adaptability to different devices. Builds trust and reflects the professionalism of a brand",
+    },
+    {
+        title: "Database Management Service",
+        description: "The database control service provides an abstraction layer between the application and the underlying database, making the data easier to access and manipulate. In addition, it often offers features such as table creation, queries, indexes, and constraints, as well as user management and access permissions.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://play-lh.googleusercontent.com/fVRg0B1Pgo0l93mSrqSnqZSNNcaqEyMGc-Y2qWqhwOE-IhYe1ogAnnFuieK9iZ8VVg",
+        description: "An app created to consult all the data of any character from the Rick and Morty series!",
+        link: "https://github.com/Tobyvallejos99/Rick_y_Morty",
+    },
+    {
+        img: "https://img.freepik.com/premium-vector/countries-flags-major-world-advanced-emerging-economies-states-official-group-twenty-flag-labels-set_102902-1882.jpg?w=2000",
+        description: "Project where you can consult various statistics of the countries of the world where you can also create tourist activities in the form of a form!",
+        link: "https://github.com/Tobyvallejos99/Individual-project---Countries",
+    },
+    {
+        img: "https://www.nme.com/wp-content/uploads/2022/03/Super-Smash-Bros-Brawl.jpg",
+        description: "...CoderGame... Marketplace",
+        link: "https://coder-game.vercel.app/",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Tobyvallejos99@gmail.com",
+    YOUR_FONE: "(011)32188982(+541132188982)",
+    description: "Do not hesitate to contact me!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Tobyvallejos99",
+    instagram: "https://www.instagram.com/tobyvallejos99/", //es el instagram
+    linkedin: "https://www.linkedin.com/in/tobiasvallejos99/",
+    twitter: "https://twitter.com/tobyvallejos99",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
